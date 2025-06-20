68e3741226b79431f992036536ec12d3444538b1
Alexey Fefelov <alexfef72@mail.ru>
2025-06-21 00:20:52 +0200

    Знакомство с Рagebject. Практика работы с Рage Оbject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,26 +1,22 @@
-from playwright.sync_api import expect, Page
 import pytest
+from pages.registration_page import RegistrationPage
+from pages.dashboard_page import DashboardPage
+
 
 @pytest.mark.registration
 @pytest.mark.regression
-# Использование фикстуры 'chromium_page', которая автоматически предоставляет готовую страницу
-def test_successfull_registration(chromium_page: Page):
-        # Теперь страница передаётся в тест через фикстуру 'chromium_page', браузер не нужно инициализировать вручную
-        chromium_page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+@pytest.mark.parametrize('email, username, password',
+        [
+                ('user.name@gmail.com', 'username', 'password'),
+        ]
+)
 
-        email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
+def test_successfull_registration(registration_page: RegistrationPage, email: str, username: str, password: str, dashboard_page: DashboardPage):
 
-        username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
+        registration_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+        registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
+        registration_page.click_registration_button()
 
-        password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-        expect(dashboard_title).to_be_visible()
+        dashboard_page.check_dashboard_title()
 
 

fc4f8acc9cadede221e6076b70242a838084c950
Alexey Fefelov <alexfef72@mail.ru>
2025-06-21 21:58:19 +0200

    Знакомство с РageОbject. Практика работы с Рage Оbject

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,18 +5,11 @@ from pages.dashboard_page import DashboardPage
 
 @pytest.mark.registration
 @pytest.mark.regression
-@pytest.mark.parametrize('email, username, password',
-        [
-                ('user.name@gmail.com', 'username', 'password'),
-        ]
-)
+def test_successfull_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
-def test_successfull_registration(registration_page: RegistrationPage, email: str, username: str, password: str, dashboard_page: DashboardPage):
-
-        registration_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-        registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
-        registration_page.click_registration_button()
-
-        dashboard_page.check_dashboard_title()
+    dashboard_page.check_dashboard_title()
 
 

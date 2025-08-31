6011b57d7b6b929071c5672f9e0a7a32d7c7a35a
Alexey Fefelov <fefelov.a@nssrb.local>
2025-08-31 19:39:47 +0200

    Практикуемся в реализации PageComponent

--- pages/create_courses_page.py
+++ pages/create_courses_page.py
@@ -1,10 +1,12 @@
 from components.navigation.navbar_component import NavbarComponent
-from components.views.empty_view_component import EmptyViewComponent
-from components.views.image_upload_widget_component import ImageUploadWidgetComponent
-from components.courses.create_course_exercise_form_component import CreateCourseExerciseFormComponent
 from components.courses.create_course_form_component import CreateCourseFormComponent
+from components.views.image_upload_widget_component import ImageUploadWidgetComponent
+from components.views.empty_view_component import EmptyViewComponent
+from components.courses.create_course_toolbar_view_component import CreateCourseToolbarViewComponent
+from components.courses.create_course_exercise_form_component import CreateCourseExerciseFormComponent
+from components.courses.create_course_exercises_toolbar_view_component import CreateCourseExercisesToolbarViewComponent
 from pages.base_page import BasePage
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
 
 
 class CreateCoursePage(BasePage):
@@ -15,42 +17,10 @@ class CreateCoursePage(BasePage):
         self.create_course_form = CreateCourseFormComponent(page)
         self.image_upload_widget = ImageUploadWidgetComponent(page, 'create-course-preview')
         self.exercises_empty_view = EmptyViewComponent(page, 'create-course-exercises')
-
+        self.create_course_toolbar_view = CreateCourseToolbarViewComponent(page)
         self.create_exercises_form = CreateCourseExerciseFormComponent(page)
+        self.create_course_exercises_toolbar = CreateCourseExercisesToolbarViewComponent(page)
 
-
-       ## self.create_course_title = page.get_by_test_id('create-course-toolbar-title-text')
-       ## self.create_course_button = page.get_by_test_id('create-course-toolbar-create-course-button')
-
-        self.exercises_title = page.get_by_test_id('create-course-exercises-box-toolbar-title-text')
-        self.exercises_button = page.get_by_test_id('create-course-exercises-box-toolbar-create-exercise-button')
-
-    def check_visible_create_course_title(self):
-        expect(self.create_course_title).to_be_visible()
-        expect(self.create_course_title).to_have_text('Create course')
-
-    def click_create_course_button(self):
-        self.create_course_button.click()
-
-    def check_visible_create_course_button(self):
-        expect(self.check_visible_create_course_button).to_be_visible()
-
-    def check_disabled_create_course_button(self):
-        expect(self.create_course_button).to_be_disabled()
-
-
-
-
-
-    def check_visible_exercises_title(self):
-        expect(self.exercises_title).to_be_visible()
-        expect(self.exercises_title).to_have_text('Exercises')
-
-    def check_visible_create_exercise_button(self):
-        expect(self.exercises_button).to_be_visible()
-
-    def click_create_exercise_button(self):
-        self.exercises_button.click()
 
     def check_visible_exercises_empty_view(self):
         self.exercises_empty_view.check_visible(
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -5,11 +5,11 @@ from pages.dashboard_page import DashboardPage
 
 @pytest.mark.registration
 @pytest.mark.regression
-def test_successfull_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.vizit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.fill_registration_form(email='user.name@gmail.com', username='username', password='password')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
     registration_page.click_registration_button()
 
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.dashboard_toolbar_view.check_visible()
 
 
